fed886f454dfd1ffa25a1a58286229a4eb785a2c
Thomas Solecki <thomasjsolecki@gmail.com>
2024-10-25 21:55:15 -0700

    Add layout and title to view

--- assets/js/app.tsx
+++ assets/js/app.tsx
@@ -2,8 +2,13 @@ import axios from "axios";
 
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
+import { createIcons, icons } from 'lucide';
+
+createIcons({ icons });
 
 axios.defaults.xsrfHeaderName = "x-csrf-token";
+
+window.document.title = "diff thing"
 
 createInertiaApp({
   resolve: async (name: string) => {
