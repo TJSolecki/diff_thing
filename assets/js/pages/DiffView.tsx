2ff41b5b8c3618707806b39116c0136eb89ff51d
Thomas Solecki <thomasjsolecki@gmail.com>
2024-10-25 22:32:44 -0700

    Add toggles for unified diff and showing diff only

--- assets/js/pages/DiffView.tsx
+++ assets/js/pages/DiffView.tsx
@@ -1,5 +1,26 @@
+import { useState } from 'react';
 import ReactDiffViewer from 'react-diff-viewer';
 
 export default function DiffView({ oldValue, newValue }: { oldValue: string, newValue: string }) {
-  return <ReactDiffViewer oldValue={oldValue} newValue={newValue} splitView={true} showDiffOnly={false} />
+  const [showUnifiedView, setShowUnifiedView] = useState<boolean>(false);
+  const [showDiffOnly, setShowDiffOnly] = useState<boolean>(false);
+
+  return <>
+    <div className="flex items-center gap-4 py-2">
+      <label className="flex gap-2 items-center">
+        <input type="checkbox" checked={showUnifiedView} onChange={event => setShowUnifiedView(event.target.checked)} />
+        Show unified diff
+      </label>
+      <label className="flex gap-2 items-center">
+        <input type="checkbox" checked={showDiffOnly} onChange={event => setShowDiffOnly(event.target.checked)} />
+        Show diff only
+      </label>
+    </div>
+    <ReactDiffViewer
+      oldValue={oldValue}
+      newValue={newValue}
+      splitView={!showUnifiedView}
+      showDiffOnly={showDiffOnly}
+    />
+  </>;
 }
